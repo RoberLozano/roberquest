484e7b5a8c99dcc47844185cb1c5c3c88fc95f0a
Rober Lozano <robertolozanosaez@gmail.com>
2024-04-15 13:06:02 +0200

    ex

--- ex.js
+++ ex.js
@@ -321,25 +321,29 @@ function arcos(inicio = 34, fin = 41, seguir = true, n = 'A', no = 'B', fue = 'C
 
     }
 
-    let daños = []
+    else{
+      let daños = []
 
-    let _daño = ws[daño + i]?.v;
-    
-    let _recto = ws[recto + i]?.v;
-    let _max = ws[max + i]?.v;
-
-
-
-    let fuerza = ws[fue + i]?.v;
-
-    let _bonAp = ws[bonAp + i]?.v;
-
-    if (_daño) {
-      daños.push(new Daño(_daño, 'P'))
+      let _daño = ws[daño + i]?.v;
+      
+      let _recto = ws[recto + i]?.v;
+      let _max = ws[max + i]?.v;
+  
+  
+  
+      let fuerza = ws[fue + i]?.v;
+  
+      let _bonAp = ws[bonAp + i]?.v;
+  
+      if (_daño) {
+        daños.push(new Daño(_daño, 'P'))
+      }
+  
+  
+      obj = new Arco(nombre, 0, 0, ...daños, _recto, _max, fuerza, _bonAp)
     }
 
 
-    obj = new Arco(nombre, 0, 0, ...daños, _recto, _max, fuerza, _bonAp)
 
     console.log(obj);
     // obj = new Arma(nombre, peso, 0, daños)
